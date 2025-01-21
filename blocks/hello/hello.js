9cc86ff25c3a2edb76aff6548872e702d63ce2a4
NehaDwivedi200 <neha.dwivedi@sciex.com>
2025-01-21 11:57:09 +0530

    add changed files

---
+++ blocks/hello/hello.js
@@ -0,0 +1,10 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  ul.innerHTML = "Hello World!"
+  block.textContent = '';
+  block.append(ul);
+}
