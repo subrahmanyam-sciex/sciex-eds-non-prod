eaf33b5d9362ef923751a91f766b7daa80a4e6f9
NehaDwivedi200 <neha.dwivedi@sciex.com>
2025-01-21 12:15:42 +0530

    build issues fixed

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -1,10 +1,7 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  ul.innerHTML = "Hello World!"
+  ul.innerHTML = 'Hello World!';
   block.textContent = '';
   block.append(ul);
 }
