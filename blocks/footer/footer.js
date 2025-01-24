b1b3dd2c1a1bb9b4ba003e7f20a2df3979c6f242
sushanth-sciex <sushanth.baddam@sciex.com>
2025-01-24 18:49:51 +0530

    Footer Fix issues

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,32 +14,4 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
   block.append(footer);
-
-  // Add navigation menu
-  const nav = document.createElement('nav');
-  nav.innerHTML = `
-    <div class='desktop-menu'>
-      <div class='menu-title'>Products</div>
-      <ul>
-        <li><a href='https://sciex.com/products/mass-spectrometers'>Mass spectrometers</a></li>
-        <li><a href='https://sciex.com/products/capillary-electrophoresis'>Capillary electrophoresis</a></li>
-        <li><a href='https://sciex.com/products/software'>Software</a></li>
-      </ul>
-    </div>
-    <div class='mobile-menu'>
-      <button onclick='toggleMenu()'>
-        <span>Products</span>
-        <svg width='14' height='8' viewBox='0 0 14 8' fill='none'>
-          <path d='M13 1L7 7L1 1' stroke='white'></path>
-        </svg>
-      </button>
-      <ul id='mobile-menu-list'>
-        <li><a href='https://sciex.com/products/mass-spectrometers'>Mass spectrometers</a></li>
-        <li><a href='https://sciex.com/products/capillary-electrophoresis'>Capillary electrophoresis</a></li>
-        <li><a href='https://sciex.com/products/software'>Software</a></li>
-      </ul>
-    </div>
-  `;
-
-  footer.prepend(nav);
 }
