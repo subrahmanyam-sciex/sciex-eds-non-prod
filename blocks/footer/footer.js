ffa82948d1d70e3667ff2a596e445e9c7d9465b9
sushanth-sciex <sushanth.baddam@sciex.com>
2025-01-27 19:25:53 +0530

    Footer Block changes for Logos

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,24 +2,44 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
+ * Loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
   block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-  // Append the footer to the block
-  block.append(footer);
 
-  // Find the h2 element and increase its font size
-  const h2 = footer.querySelector('h2');
+  let iteration = 0;
+  let firstSection = true;
+
+  while (fragment.firstElementChild) {
+    iteration += 1; // Fix: Using += 1 instead of ++
+    const wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add(`wrapper-${iteration}`);
+
+    // Add 'footer-list' class to the first section
+    if (firstSection) {
+      wrapperDiv.classList.add('footer-list');
+    } else {
+      wrapperDiv.classList.add('black-section');
+    }
+
+    wrapperDiv.append(fragment.firstElementChild);
+    block.append(wrapperDiv);
+
+    if (iteration === 2) {
+      const hr = document.createElement('hr');
+      hr.classList.add('footer-divider');
+      block.insertBefore(hr, wrapperDiv);
+    }
+
+    firstSection = false;
+  }
+
+  const h2 = block.querySelector('.wrapper-1 h2');
   if (h2) {
     h2.style.fontSize = '145px';
   }
