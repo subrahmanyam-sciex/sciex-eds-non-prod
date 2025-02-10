7273696ea04195e93342be3716fa3d61a5fc8bd1
Subrahmanyam Yerukonda <>
2025-02-10 18:10:30 +0530

    Mega-menu sub nav changes.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,7 +1,9 @@
 import {
-  div, span, ul, li,
+  div, span, ul, li, a,
 } from '../../scripts/dom-builder.js';
+import { decorateIcons } from '../../scripts/aem.js';
 
+const menuLinks = {};
 function createMainHeader(section) {
   const menuDiv = div({ class: 'tw-flex tw-w-full tw-bg-grey-900 tw-text-grey-300 tw-z-[100] tw-relative' });
   const containerDiv = div({ class: 'tw-flex tw-container' });
@@ -54,21 +56,240 @@ function createMegaMenuTopNav(section) {
   const border = document.createElement('div');
   border.className = 'tw-border-b  tw-flex tw-items-center desktop-links ';
   const ulTag = document.createElement('ul');
-  ulTag.className = 'tw-list-none tw-inline-flex stretch-text tw-text-grey-900';
+  ulTag.className = 'mainmenu-ul tw-list-none tw-inline-flex stretch-text tw-text-grey-900';
 
   const elements = section.querySelector('ul').children;
   Array.from(elements).forEach((element) => {
     element.className = 'tw-py-20 tw-border-b-2 tw-border-white tw-ml-24';
+    element.id = element.firstChild.text.replace(/ /g, '-');
     element.firstChild.className = 'tw-flex tw-items-center tw-group hover:tw-text-blue-700 tw-transition-colors';
     const svg = '<span class="tw-ml-6"><svg width="14" height="8" viewBox="0 0 14 8" fill="none"><path d="M13 1L7 7L1 0.999999" stroke="currentColor"></path></svg></span>';
     if (element.firstChild) {
       element.firstChild.insertAdjacentHTML('beforeend', svg);
     }
     ulTag.append(element);
+    element.addEventListener('click', (event) => {
+      const menuId = event.currentTarget.id;
+      const maninNavliList = document.querySelectorAll('.mainmenu-ul li');
+      Array.from(maninNavliList).forEach((ele) => {
+        ele.classList.remove('menu-active');
+      });
+      event.currentTarget.classList.add('menu-active');
+      if (menuLinks[menuId] && menuLinks[menuId].length > 0) {
+        const firstLi = menuLinks[menuId][0].firstElementChild.text;
+        if (firstLi) {
+          const className = firstLi.trim().replace(/ /g, '-').toLowerCase();
+          const myDiv = document.getElementById(`submenu-${className}`);
+          if (myDiv && myDiv.style.display === 'none') {
+          // hiding current div
+            const activediv = document.querySelector('div .secondnav-active');
+            if (activediv) {
+              activediv.classList.remove('secondnav-active');
+              const liList = activediv.querySelectorAll('ul li');
+              Array.from(liList).forEach((elem) => {
+                if (elem.querySelector('a .submenu-active') && elem.querySelector('a .submenu-active') === 'submenu-active') {
+                  elem.querySelector('a').classList.remove('submenu-active');
+                }
+              });
+              activediv.style.display = 'none';
+            }
+            myDiv.querySelector('ul li a').classList.add('submenu-active');
+            myDiv.classList.add('secondnav-active');
+            myDiv.style.display = '';
+          } else {
+            const activediv = document.querySelector('div .secondnav-active');
+            if (activediv) {
+              activediv.querySelector('ul li a').classList.remove('submenu-active');
+              activediv.classList.remove('secondnav-active');
+              activediv.style.display = 'none';
+            }
+          }
+        }
+      }
+    });
   });
   border.append(ulTag);
   container.append(border);
   parentDiv.append(container);
+  return parentDiv;
+}
+
+function createMegaMenuSubNav(child) {
+  // Select all section headings (e.g., "Products", "Applications", etc.)
+  const sections = child.querySelectorAll('p');
+  // let menuLinks = {};
+
+  sections.forEach((section) => {
+    const sectionTitle = section.textContent.trim().replace(/ /g, '-');
+    const list = section.nextElementSibling; // The associated UL element
+    const subMenuLinks = [];
+    list.querySelectorAll('ul > li').forEach((childMenuItem) => {
+      subMenuLinks.push(childMenuItem);
+    });
+    menuLinks[sectionTitle] = subMenuLinks;
+  });
+}
+
+function createSubMenuItems(section, containerDiv, firstpartdiv) {
+  const sectionTitle = section.textContent.trim();
+  if (sectionTitle.includes('|')) {
+    const nameArray = sectionTitle.split('|');
+    const productTitle = nameArray[0].trim().replace(/ /g, '-');
+    const subNavClass = nameArray[1].trim().replace(/ /g, '-').toLowerCase();
+    const secondLevelList = menuLinks[productTitle];
+    // First part preparation
+    const ulTag = document.createElement('ul');
+    ulTag.className = `${subNavClass}-ul`;
+    containerDiv.id = `submenu-${subNavClass}`;
+    if (secondLevelList) {
+      Array.from(secondLevelList).forEach((element) => {
+        const liEle = document.createElement('li');
+        const litext = element.firstChild.text;
+        const liClass = litext.trim().replace(/ /g, '-').toLowerCase();
+        liEle.className = liClass;
+        liEle.append(a(
+          {
+            class: 'tw-relative submenu tw-font-light tw-text-lg tw-text-grey-900 tw-flex tw-items-center tw-justify-between tw-w-full tw-group hover:tw-text-blue-700 tw-transition-all tw-duration-200',
+            title: subNavClass,
+            href: '#',
+          },
+          span({ class: 'tw-mr-3' }, `${element.firstChild.text}`),
+        ));
+        ulTag.append(liEle);
+
+        liEle.addEventListener('click', (event) => {
+          const liClassName = event.currentTarget.className;
+          const myDiv = document.getElementById(`submenu-${liClassName}`);
+          if (myDiv && myDiv.style.display === 'none') {
+            // hiding current div
+            const activediv = document.querySelector('div .secondnav-active');
+            activediv.querySelector('ul li a').classList.remove('submenu-active');
+            activediv.classList.remove('secondnav-active');
+            activediv.style.display = 'none';
+            // showing selected div
+            const liList = myDiv.querySelectorAll('ul li');
+            Array.from(liList).forEach((ele) => {
+              if (ele.className === liClassName) {
+                ele.querySelector('a').classList.add('submenu-active');
+              }
+            });
+            myDiv.classList.add('secondnav-active');
+            myDiv.style.display = '';
+          } else {
+            const activediv = document.querySelector('div .secondnav-active');
+            activediv.style.display = 'none';
+          }
+        });
+      });
+      firstpartdiv.append(ulTag);
+    }
+  }
+}
+
+function createMegaMenuThirdLevel(child) {
+  const parentDiv = div({ class: 'tw-w-full tw-bg-white tw-relative tw-z-[100]' });
+  const containerDiv = document.createElement('div');
+  containerDiv.className = 'submenu-container tw-hidden lg:tw-flex tw-container tw-pt-32 tw-pb-40 tw-columns-12';
+  containerDiv.style.display = 'none';
+  const sections = child.querySelectorAll('p');
+  const isImages = child.querySelectorAll('img');
+
+  // First part preparation
+  const firstpartdiv = div({ class: 'tw-w-3/12 tw-border-r tw-relative tw-pr-24' });
+
+  const secondPartdiv = div({ class: 'tw-w-6/12 tw-px-32 tw-pr-40 tw-border-r' });
+  const wrapdiv = div({ class: 'tw-flex tw-flex-wrap tw-h-fit' });
+
+  const thirdPartdiv = div({ class: 'tw-w-3/12 tw-pl-32' });
+  if (sections && sections.length > 0 && isImages && isImages.length > 0) {
+    sections.forEach((section, index) => {
+      if (index === 0) {
+        createSubMenuItems(section, containerDiv, firstpartdiv);
+      } else {
+        const list = section.querySelectorAll('a');
+        if (list.length > 0 && section.querySelector('strong')) {
+          const listDiv = div({ class: 'lg:tw-w-full xl:tw-w-1/2 tw-pr-48 tw-mt-24' });
+          Array.from(list).forEach((element, ind) => {
+            element.className = 'tw-group';
+            const spanTag = span({ class: 'tw-text-mobBase md:tw-text-base tw-text-grey-500 tw-flex tw-items-center group-hover:tw-text-blue-700 tw-transition-all tw-duration-200 ' }, element.text);
+            if (ind === 0) {
+              spanTag.classList.add('tw-font-bold');
+              spanTag.classList.replace('tw-text-grey-500', 'tw-text-grey-900');
+            }
+            element.text = '';
+            element.append(spanTag);
+            const chevronRight = span({ class: 'icon icon-chevron-right tw-ml-8 tw-duration-500 group-hover:tw-pl-2' });
+            spanTag.append(chevronRight);
+            listDiv.append(element);
+          });
+          wrapdiv.append(listDiv);
+        } else {
+          const img = section.querySelector('img');
+          const listDiv = div({ class: '' });
+
+          if (img != null) {
+            const picDiv = div({ class: 'tw-relative tw-overflow-hidden tw-pt-[56.25%]' });
+            img.className = 'tw-transition-all tw-duration-500 tw-absolute tw-inset-0 tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover hover:tw-scale-[1.05]';
+            picDiv.append(img);
+            listDiv.append(picDiv);
+
+            thirdPartdiv.append(listDiv);
+            // thirdPartdiv.append(document.createElement('br'));
+          }
+          if (section.previousElementSibling.querySelector('picture')) {
+            const anchTag = section.querySelector('a');
+            if (anchTag) {
+              const spanTag = span({ class: 'tw-block tw-pt-16 tw-text-grey-900 tw-font-bold tw-flex tw-items-center group-hover:tw-text-blue-700 tw-transition-colors' }, anchTag.text);
+              const chevronRight = span({ class: 'icon icon-chevron-right tw-ml-8 tw-duration-500 group-hover:tw-pl-2' });
+              spanTag.append(chevronRight);
+              anchTag.text = '';
+              anchTag.append(spanTag);
+              const pTag = section.nextElementSibling;
+              pTag.className = 'tw-mt-2 tw-text-grey-500 tw-text-sm tw-mb-0';
+              anchTag.append(pTag);
+              listDiv.append(anchTag);
+              thirdPartdiv.append(listDiv);
+              thirdPartdiv.append(document.createElement('br'));
+            }
+          }
+        }
+      }
+    });
+  } else {
+    secondPartdiv.className = 'tw-w-9/12 tw-px-32 tw-pr-40';
+    wrapdiv.className = 'tw-flex tw-flex-wrap tw-h-fit';
+    sections.forEach((section, index) => {
+      if (index === 0) {
+        createSubMenuItems(section, containerDiv, firstpartdiv);
+      } else {
+        const list = section.querySelectorAll('a');
+        if (list.length > 0 && section.querySelector('strong')) {
+          const listDiv = div({ class: 'tw-w-1/2 xl:tw-w-1/3 tw-pr-48 tw-mt-24 xl:tw-mt-0 lg:tw-mt-0' });
+          Array.from(list).forEach((element, ind) => {
+            element.className = 'tw-group';
+            const spanTag = span({ class: 'tw-text-mobBase md:tw-text-base tw-text-grey-500 tw-flex tw-items-center group-hover:tw-text-blue-700 tw-transition-all tw-duration-200 ' }, element.text);
+            if (ind === 0) {
+              spanTag.classList.add('tw-font-bold');
+              spanTag.classList.replace('tw-text-grey-500', 'tw-text-grey-900');
+            }
+            element.text = '';
+            element.append(spanTag);
+            const chevronRight = span({ class: 'icon icon-chevron-right tw-ml-8 tw-duration-500 group-hover:tw-pl-2' });
+            spanTag.append(chevronRight);
+            listDiv.append(element);
+          });
+          wrapdiv.append(listDiv);
+        }
+      }
+    });
+  }
+  secondPartdiv.append(wrapdiv);
+  containerDiv.appendChild(firstpartdiv);
+  containerDiv.appendChild(secondPartdiv);
+  if (isImages.length > 0) {
+    containerDiv.appendChild(thirdPartdiv);
+  }
+  parentDiv.append(containerDiv);
   return parentDiv;
 }
 
@@ -86,11 +307,12 @@ function processHtml(block, main) {
       nav.append(createMainHeader(section));
     } else if (iteration === 2) {
       nav.append(createMegaMenuTopNav(section));
+    } else if (iteration === 3) {
+      createMegaMenuSubNav(section);
     } else {
-      // handleBlockSection(child, block, iteration);
+      nav.appendChild(createMegaMenuThirdLevel(section));
     }
   });
-
   parentDiv.append(nav);
   block.prepend(parentDiv);
 }
@@ -109,4 +331,5 @@ export default async function decorate(block) {
     main.innerHTML = html;
     processHtml(block, main);
   }
+  decorateIcons(block);
 }
